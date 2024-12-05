106c87736a3d1eea7efa7a27e20281706d113683
GiovanniTammaro9 <gio_play@live.it>
2024-12-05 16:20:17 +0100

    innertext

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function modificaElemento() {
     let titolo = document.querySelector("#titolo");
     titolo.innerHTML = "il testo è stato cambiato";
 }
+
+//innertext
+let paragrafo = document.createElement("p");
+paragrafo.innerText = "questo è un paragrafo iniziale";
+
+document.body.appendChild(paragrafo);
+function modificaTesto() {
+    paragrafo.innerText = "il testo del paragrafo è stato modificato";
+}
