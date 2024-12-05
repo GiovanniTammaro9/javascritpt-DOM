2d23d1b5ed28280c5dee912a0075f2a0606ce812
GiovanniTammaro9 <gio_play@live.it>
2024-12-05 16:11:25 +0100

    queryselector

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+//getelementbyid
 function modificaElemento() {
     let titolo = document.getElementById("titolo");
+    titolo.innerHTML = "il testo è stato cambiato";
+}
+//queryselector
+function modificaElemento() {
+    let titolo = document.querySelector("#titolo");
     titolo.innerHTML = "il testo è stato cambiato";
 }
